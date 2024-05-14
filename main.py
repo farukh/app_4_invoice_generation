1e107d30487f192794230e579dc95ca330cb665b
FarukhMushtaq <farukhmushtaq@apesquared.com>
2024-05-14 17:08:20 +0500

    - Added Invoice date in PDF under Invoice Date

--- main.py
+++ main.py
@@ -11,9 +11,11 @@ for filepath in filepaths:
     print(filepath)
     print(df)
     filename = Path(filepath).stem #extract filename from path
-    invoice_nr = filename.split("-")[0]
+    invoice_nr, date = filename.split("-")
     pdf = FPDF(orientation='P', unit='mm', format='A4')
     pdf.add_page()
     pdf.set_font(family='Times',size=12,style='B')
-    pdf.cell(w=50,h=8,txt=f'Invoice nr.{invoice_nr}')
+    pdf.cell(w=50,h=8,txt=f'Invoice nr.{invoice_nr}',ln=1)
+    pdf.cell(w=50,h=8,txt=f'Date: {date}')
+
     pdf.output(f"PDFs/{filename}.pdf")
