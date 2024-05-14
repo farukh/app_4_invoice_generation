453e2bfd4df2ca6c10efc11553abb8b5920669b1
FarukhMushtaq <farukhmushtaq@apesquared.com>
2024-05-14 17:43:27 +0500

    - Added Headers - Added Table data

--- main.py
+++ main.py
@@ -6,16 +6,35 @@ from pathlib import Path
 filepaths = glob.glob("invoices/*.xlsx")
 print(filepaths)
 
+
 for filepath in filepaths:
-    df = pd.read_excel(filepath,sheet_name='Sheet 1') #install openpyxl package
     print(filepath)
-    print(df)
     filename = Path(filepath).stem #extract filename from path
     invoice_nr, date = filename.split("-")
     pdf = FPDF(orientation='P', unit='mm', format='A4')
     pdf.add_page()
     pdf.set_font(family='Times',size=12,style='B')
-    pdf.cell(w=50,h=8,txt=f'Invoice nr.{invoice_nr}',ln=1)
-    pdf.cell(w=50,h=8,txt=f'Date: {date}')
+    pdf.cell(w=50,h=8,txt=f'Invoice nr.{invoice_nr}', ln=1)
+    pdf.cell(w=50,h=8,txt=f'Date: {date}', ln=2)
+    df = pd.read_excel(filepath,sheet_name='Sheet 1') #install openpyxl package
 
+    columns = list(df.columns)
+    print(columns)
+    columns = [item.replace('_',' ').title() for item in columns]
+
+    pdf.set_font(family='Times', size=10,style='B' )
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=10, txt=f"{columns[0]}", border=1)
+    pdf.cell(w=70, h=10, txt=f"{columns[1]}", border=1)
+    pdf.cell(w=30, h=10, txt=f"{columns[2]}", border=1)
+    pdf.cell(w=30, h=10, txt=f"{columns[3]}", border=1)
+    pdf.cell(w=30, h=10, txt=f"{columns[4]}", ln=1, border=1)
+    for index, row in df.iterrows():
+        pdf.set_font(family='Times', size=10, )
+        pdf.set_text_color(80,80,80)
+        pdf.cell(w=30, h=10, txt=f"{row['product_id']}",border=1)
+        pdf.cell(w=70, h=10, txt=f"{row['product_name']}",border=1)
+        pdf.cell(w=40, h=10, txt=f"{row['amount_purchased']}",border=1)
+        pdf.cell(w=30, h=10, txt=f"{row['price_per_unit']}",border=1)
+        pdf.cell(w=30, h=10, txt=f"{row['total_price']}",ln=1,border=1)
     pdf.output(f"PDFs/{filename}.pdf")
