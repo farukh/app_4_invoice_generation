a31af59a50dc877a293a74b37bc4fec1482a7c75
FarukhMushtaq <farukhmushtaq@apesquared.com>
2024-05-14 19:54:22 +0500

    - Added Headers - Added Price - Total - Company Logo

--- main.py
+++ main.py
@@ -22,19 +22,41 @@ for filepath in filepaths:
     print(columns)
     columns = [item.replace('_',' ').title() for item in columns]
 
-    pdf.set_font(family='Times', size=10,style='B' )
+    pdf.set_font(family='Times', size=10,style='B')
     pdf.set_text_color(80, 80, 80)
     pdf.cell(w=30, h=10, txt=f"{columns[0]}", border=1)
-    pdf.cell(w=70, h=10, txt=f"{columns[1]}", border=1)
-    pdf.cell(w=30, h=10, txt=f"{columns[2]}", border=1)
+    pdf.cell(w=60, h=10, txt=f"{columns[1]}", border=1)
+    pdf.cell(w=40, h=10, txt=f"{columns[2]}", border=1)
     pdf.cell(w=30, h=10, txt=f"{columns[3]}", border=1)
     pdf.cell(w=30, h=10, txt=f"{columns[4]}", ln=1, border=1)
+
     for index, row in df.iterrows():
         pdf.set_font(family='Times', size=10, )
-        pdf.set_text_color(80,80,80)
-        pdf.cell(w=30, h=10, txt=f"{row['product_id']}",border=1)
-        pdf.cell(w=70, h=10, txt=f"{row['product_name']}",border=1)
-        pdf.cell(w=40, h=10, txt=f"{row['amount_purchased']}",border=1)
-        pdf.cell(w=30, h=10, txt=f"{row['price_per_unit']}",border=1)
-        pdf.cell(w=30, h=10, txt=f"{row['total_price']}",ln=1,border=1)
+        pdf.set_text_color(80, 80, 80)
+        pdf.cell(w=30, h=10, txt=f"{row['product_id']}", border=1)
+        pdf.cell(w=60, h=10, txt=f"{row['product_name']}", border=1)
+        pdf.cell(w=40, h=10, txt=f"{row['amount_purchased']}", border=1)
+        pdf.cell(w=30, h=10, txt=f"{row['price_per_unit']}", border=1)
+        pdf.cell(w=30, h=10, txt=f"{row['total_price']}", ln=1, border=1)
+
+# Sum of Total Price Column
+    sum_of_total = sum(df['total_price'])
+
+    pdf.set_font(family='Times', size=10, style='B')
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=10, txt=f"", border=1)
+    pdf.cell(w=60, h=10, txt=f"", border=1)
+    pdf.cell(w=40, h=10, txt=f"", border=1)
+    pdf.cell(w=30, h=10, txt=f"Total", border=1)
+    pdf.cell(w=30, h=10, txt=f"{sum_of_total}", border=1, ln=1)
+
+    pdf.set_font(family='Times', size=12, style='B')
+    pdf.cell(w=30, h=10, txt=f"The total price is : {sum_of_total}",  ln=1)
+    pdf.set_font(family='Times', size=12, style='B')
+
+    pdf.cell(w=40, h=8, txt=f"PythonHow ")
+    pdf.image('pythonhow.png',w=10)
+
+
     pdf.output(f"PDFs/{filename}.pdf")
+
